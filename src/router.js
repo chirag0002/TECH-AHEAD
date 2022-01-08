648256ee4818ac0723f415239c4c89225db07128
chirag0002 <varshneychirag34@gmail.com>
2022-01-08 14:27:25 +0530

    Again Responsive

--- src/router.js
+++ src/router.js
@@ -3,13 +3,8 @@ import { Route, ReactLocation } from 'react-location';
 import App from './App';
 import DevOps from './Components/DevOps/DevOps'
 import Blockchain from './Components/BlockChain';
-import Headder from './Components/Header';
-
-
-import Navbar from './Components/Navbar';
-// import Footer from './containers/footer/Footer';
 import Ai from './Components/Artificial-Intelligence/Ai';
-// import BlockChain from './components/Technologies/BlockChain';
+
 
 export const routes: Route[] = [
   {
@@ -25,30 +20,9 @@ export const routes: Route[] = [
     element: <Blockchain />,
   },
   {
-<<<<<<< HEAD
-    path: '/headder',
-    element: <Headder />,
-  },
-  {
-    path: 'nav',
-    element: <Navbar />,
-=======
     path: 'devops',
     element: <DevOps />,
->>>>>>> upstream/main
   },
-  // {
-  //   path: 'check',
-  //   element: <Footer />,
-  // },
-  // {
-  //   path: 'ai',
-  //   element: <Ai />,
-  // },
-  // {
-  //   path: 'blockchain',
-  //   element: <BlockChain />,
-  // },
 ];
 export const location = new ReactLocation();
 
