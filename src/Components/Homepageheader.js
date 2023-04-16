c00e2621093fbcde8272d2963b95b586f0ede941
chirag0002 <varshneychirag34@gmail.com>
2023-04-16 23:42:35 +0530

    file changed

--- src/Components/Homepageheader.js
+++ src/Components/Homepageheader.js
@@ -8,7 +8,7 @@ const Homepageheader = () =>{
     return(
   <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
-      <h1 className="gradient__text">Let&apos;s Explore Every Tech Which Lies Ahead With Us.</h1>
+      <h1 className="gradient__text">Let&apos;s Explore Every Tech Which Lies Ahead Us.</h1>
       <p>If future generations are to remember us more with gratitude than sorrow, we must achieve more than just the miracles of technology. We must also leave them a glimpse of the world as it was created, not just as it looked when we got through with it.
         <br/>
         <br/>
